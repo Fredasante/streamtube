93e375664ba6f18c845697d2cbcfd6fb5633272b
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2023-11-01 12:41:35 +0000

    Header fix

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
       }}
     >
       <Link href="/" style={{ display: "flex", alignItems: "center" }}>
-        <Image
+        <img
           src="/capture.png"
           height={45}
           width={45}
