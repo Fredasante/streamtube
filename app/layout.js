e6e69ed7b202fd605f68190aa134bf688a5b8fbd
Alfred Asante Apenteng <alfredapenteng6996@gmail.com>
2024-01-19 10:15:35 +0000

    title change

--- app/layout.js
+++ app/layout.js
@@ -5,8 +5,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Youtube Clone",
-  description: "Clone of youtube website",
+  title: "Streamtube",
+  description: "A video sharing website",
 };
 
 export default function RootLayout({ children }) {
